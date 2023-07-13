17c930b2a450be5d8a8ab3af535a04337a496762
nguyenvuthien <thien180102@gmail.com>
2023-07-13 17:16:40 +0700

    vue 3

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -17,7 +17,7 @@
             <div class="card__face card__face--back">
                 <div class="card__content" 
                     :style="{
-                        backgroundImage: `url(../src/assets/${imgBackFaceUrl})`,
+                        backgroundImage: `url(../assets/${imgBackFaceUrl})`,
                     }"
                 ></div>
             </div>  
